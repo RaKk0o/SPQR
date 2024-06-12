d787b777c0672c9b2eb13926bae4487c5f271254
Clémence Faligot <75119672+RaKk0o@users.noreply.github.com>
2024-06-12 15:53:53 +0200

    try new help

--- bot.py
+++ bot.py
@@ -10,7 +10,16 @@ intents = discord.Intents.default()
 intents.messages = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+class CustomHelpCommand(commands.HelpCommand):
+    async def send_bot_help(self, mapping):
+        embed = discord.Embed(title="Help", description="Voici une liste de commandes disponibles :")
+        for cog, commands in mapping.items():
+            for command in commands:
+                embed.add_field(name=f"!{command.name}", value=command.help or "No description", inline=False)
+        channel = self.get_destination()
+        await channel.send(embed=embed)
+
+bot = commands.Bot(command_prefix='!', intents=intents, help_command=CustomHelpCommand())
 
 @bot.event
 async def on_ready():

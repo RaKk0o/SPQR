082c30beee62f6ed84cb719d43df1b8b48218abc
Clémence Faligot <75119672+RaKk0o@users.noreply.github.com>
2024-06-12 15:45:55 +0200

    add help

--- bot.py
+++ bot.py
@@ -9,3 +9,13 @@ load_dotenv()
 intents = discord.Intents.default()
 intents.messages = True
 intents.message_content = True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Bot connecté en tant que {bot.user}')
+
+@bot.command(name='help')
+async def hello(ctx):
+    await ctx.send('Les commandes disponibles sont:')
